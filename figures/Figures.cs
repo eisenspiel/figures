6afeba1b7b6e7cf9f4e8d78c1bf894b0999b92b2
eisenspiel <eisenspiel@gmail.com>
2021-04-18 21:04:11 +0300

    Add project files.

---
+++ figures/Figures.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figures
+{
+    public static class Circle
+    {
+        //Вычисляет площадь круга
+        public static double GetArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public static double GetArea(int radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    public static class Triangle
+    {
+        //Вычисляет площадь треугольника
+        public static double GetArea(double a, double b, double c)
+        {
+            double p = a + b + c;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public static double GetArea(int a, int b, int c)
+        {
+            double p = a + b + c;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        //Проверяет, является ли треугольник прямоугольным
+        public static bool IsRight(double a, double b, double c)
+        {
+            return ( (a*a == b*b + c*c) || (b * b == a * a + c * c )|| (c * c == a * a + b * b));
+        }
+
+        public static bool IsRight(int a, int b, int c)
+        {
+            return ((a * a == b * b + c * c) || (b * b == a * a + c * c) || (c * c == a * a + b * b));
+        }
+    }
+}
---
+++ figures/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Figures;
+
+namespace figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = 10.5;
+            Console.WriteLine($"Площадь круга с радиусом {r} равна {Math.Round(Circle.GetArea(r), 2)}");
+            Console.WriteLine($"Площадь круга с радиусом {(int)r} равна {Math.Round(Circle.GetArea((int)r), 2)}");
+            Console.WriteLine();
+
+            int a = 3;
+            int b = 4;
+            int c = 5;
+            double area1 = Math.Round(Triangle.GetArea(a, b, c), 2);
+
+            Console.WriteLine($"Площадь треугольника со сторонами {a}, {b}, {c} равна {area1}");
+            if (Triangle.IsRight(a, b, c)) Console.WriteLine("Это прямоугольный треугольник");
+            else Console.WriteLine("Это не прямоугольный треугольник");
+            Console.WriteLine();
+
+            double x = 3.5;
+            double y = 8.0;
+            double z = 14.4;
+            double area2 = Math.Round(Triangle.GetArea(x, y, z), 2);
+
+            Console.WriteLine($"Площадь треугольника со сторонами {x}, {y}, {z}" +
+                $" равна {area2}");
+            if (Triangle.IsRight(x, y, z)) Console.WriteLine("Это прямоугольный треугольник");
+            else Console.WriteLine("Это не прямоугольный треугольник");
+        }
+    }
+}
